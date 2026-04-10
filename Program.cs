2b9b57194e6dcb2e4963d61e8047e6837449e14c
windakarola <windakarola@gmail.com>
2026-04-10 19:23:43 +0700

    Add basic arithmetic functions and output results

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+int a = 10;
+int b = 16;
+
+static int Penjumlahan(int var1, int var2)
+{
+    return var1 + var2;
+}
+
+static int Pengurangan(int var1, int var2)
+{
+    return var1 - var2;
+}
+
+static int Perkalian(int var1, int var2)
+{
+    return var1 * var2;
+}
+
+static int Pembagian(int var1, int var2)
+{
+    return var1 / var2;
+}
+
+Console.WriteLine(a + "+" + b + "=" + Penjumlahan(a, b));
+Console.WriteLine(a + "-" + b + "=" + Pengurangan(a, b));
+Console.WriteLine(a + "*" + b + "=" + Perkalian(a, b));
+Console.WriteLine(a + "/" + b + "=" + Pembagian(a, b));
